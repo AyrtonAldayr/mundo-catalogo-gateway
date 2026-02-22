b33e6af099a4d5e1bf5dfa9ca717b2d5b77d1f44
AyrtonAldayr <ayrton.24.6.95@gmail.com>
2026-02-22 00:31:29 -0500

    Initialize Spring Boot project with Maven wrapper, basic application structure, and configuration files. Added .gitignore, .gitattributes, and VS Code settings. Included application properties and test class for gateway application.

---
+++ src/main/java/com/mundo/catalogo/gateway/MundoCatalogoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mundo.catalogo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundoCatalogoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundoCatalogoGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mundo/catalogo/gateway/MundoCatalogoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mundo.catalogo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundoCatalogoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
